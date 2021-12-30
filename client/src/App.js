d192d96c88a72016d91c206d04258e8008c9fc23
KhalilBenMansour <khalil.benmansour@enis.tn>
2021-12-30 17:41:57 +0100

    Update notFound route

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path="" element={<Boards />} />
           <Route path="/boards/:id" element={<Board />} />
         </Route>
-        <Route path="/*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
